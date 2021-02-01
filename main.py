7c2e33020566c7cb1b11ae18c6313f49c20bad63
Albert <62185496+ex1tt@users.noreply.github.com>
2021-02-01 18:18:27 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,103 @@
+# WEATHER APP
+# Made by Albert Murphy [ex1tt]
+# Feel free to use this
+# You should get your own API
+# https://github.com/ex1tt
+
+import tkinter as tk
+import requests
+import json
+
+root = tk.Tk()
+root.geometry("400x200")
+root.title("Weather App")
+root.resizable(0, 0)
+
+#  All Locations
+locations = ['Cork', 'Dublin', 'London', 'Paris']
+
+#  Creating variables before defined in functions
+api_key = ''
+temperature = ''
+weather = ''
+timezone = ''
+
+#  ALL API KEYS
+api_key_cork = 'http://api.openweathermap.org/data/2.5/weather?q=Cork&appid=94a593b645d0ba85af5528ac0d802f52'
+
+api_key_dublin = 'http://api.openweathermap.org/data/2.5/weather?q=Dublin&appid=94a593b645d0ba85af5528ac0d802f52'
+
+api_key_london = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=94a593b645d0ba85af5528ac0d802f52'
+
+api_key_paris = 'http://api.openweathermap.org/data/2.5/weather?q=Paris&appid=94a593b645d0ba85af5528ac0d802f52'
+
+
+#  Running all functions together
+def run_all_functions():
+    get_api_key()
+    get_temp_and_location()
+    show_information()
+
+
+#  Create Function To Choose API KEY/Location
+def get_api_key():
+
+    global api_key
+
+    location_chosen_get = location_chosen.get()
+
+    if str(location_chosen_get) == 'Cork':
+        api_key = api_key_cork
+    elif str(location_chosen_get) == 'Dublin':
+        api_key = api_key_dublin
+    elif str(location_chosen_get) == 'London':
+        api_key = api_key_london
+    elif str(location_chosen_get) == 'Paris':
+        api_key = api_key_paris
+
+
+#  Create Function To Show Temp Of Chosen Location
+def get_temp_and_location():
+
+    global temperature
+    global weather
+
+    api_request = requests.get(api_key)
+
+    api = json.loads(api_request.content)
+
+    base_temperature = float(api['main']['temp']) - 273.15
+
+    converted_temperature = round(base_temperature, 1)
+
+    temperature = "Temperature: " + str(converted_temperature) + "°C"
+
+    #  Get the weather
+    base_weather = api['weather'][0]['main']
+    weather = "Weather: " + base_weather
+
+
+#  put all the information on the screen
+def show_information():
+
+    temp_lbl = tk.Label(root, text=temperature, font=('calibri', 15), width=30,  anchor='w', bg='white')
+    temp_lbl.place(x=100, y=150)
+
+    location_lbl = tk.Label(root, text=weather, font=('calibri', 15), width=30, anchor='w', bg='white')
+    location_lbl.place(x=100, y=100)
+
+
+#  Create Button/OptionMenu To Choose Location
+location_chosen = tk.StringVar(root)
+location_chosen.set(locations[0])
+
+location_option_menu = tk.OptionMenu(root, location_chosen, *locations)
+location_option_menu.configure(font=('calibri', 15), width=6)
+location_option_menu.place(x=120, y=20)
+
+location_btn = tk.Button(root, command=run_all_functions)
+location_btn.configure(font=('calibri', 13), width=2)
+location_btn.place(x=230, y=21)
+
+
+root.mainloop()
